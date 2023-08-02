3816e5a90aa29e0a48bfe16e3569d620e6357dfd
oluceps <ospp@oluceps.uk>
2023-08-02 04:37:36 +0000

    fix: build script path error
    Signed-off-by: oluceps <ospp@oluceps.uk>

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/jprendes/emception.git
+pushd emception
 git reset --hard da323f97400492fcfdaef2dd7eaf3868f60c26b4
-
-pushd ./emception
 . ./build-with-docker.sh
 popd
+
+pnpm build
 echo "Finish."
